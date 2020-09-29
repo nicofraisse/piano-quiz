cda6b2cd7c184d34c4bae0d6efe1372e206829a5
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-09-29 12:38:58 -0400

    piano component can now play on its own without having to be related to a quiz game

--- src/components/Piano/Piano.js
+++ src/components/Piano/Piano.js
@@ -2,28 +2,37 @@
 import React from 'react';
 import { Howler } from 'howler';
 
+// Utility functions
+import soundPlay from '../../util/soundPlay';
+
 // Components
 import PianoNote from './PianoNote/PianoNote';
 
+// Data
+import audioClips from '../../data/audioClips'
+
 
 const Piano = (props) => {
+  const pressNote = (note) => {
+    soundPlay(audioClips[note]);
+    if (props.quiz) {
+      props.sendNoteToGame(note);
+    }
+  }
+
   Howler.volume(0.9) // can make component to adjust volume
 
-  const piano = Object.keys(props.notes).map((note, index) =>
+  const piano = Object.keys(audioClips).map((note, index) =>
     <PianoNote
       key={index}
-      click={() => props.pressNote(note)}
+      click={() => pressNote(note)}
       label={note}
     />
   );
 
   return (
     <div>
-      <button onClick={props.clickStart}>Start game</button>
-      <button onClick={props.clickPlay}>Play notes</button>
-      <h2>Round {props.currentSubquizIndex + 1}/{props.quiz.quizzes.length}</h2>
       {piano}
-      <h3>{props.gameFinished ? "Game finished" : null}</h3>
     </div>
   );
 }
