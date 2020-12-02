7bca69b54a84549afb716fc79958437a5780867a
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-12-02 09:25:12 -0500

    preload key sounds

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
 // Libraries
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 // Data
-import loadDatabase from './data/loadDatabase';
+import loadDatabase from './data/loadDatabase'
 
 // Layouts
-import Game from './layouts/Game/Game';
-import Dashboard from './layouts/Dashboard/Dashboard';
-import LandingPage from './layouts/LandingPage/LandingPage';
-import Freeplay from './layouts/Freeplay/Freeplay';
+import Game from './layouts/Game/Game'
+import Dashboard from './layouts/Dashboard/Dashboard'
+import LandingPage from './layouts/LandingPage/LandingPage'
+import Freeplay from './layouts/Freeplay/Freeplay'
 
 // Components
-import Navbar from './components/UI/Navbar/Navbar';
+import Navbar from './components/UI/Navbar/Navbar'
 
 // Stylesheets
-import './App.css';
+import './App.css'
 
 const App = () => {
-  const [ dashboardShowing, setDashboardShowing ] = useState(false);
-  const [ freeplayShowing, setFreeplayShowing ] = useState(false);
-  const [ createQuizShowing, setCreateQuizShowing ] = useState(false);
-  const [ activeQuiz, setActiveQuiz ] = useState(null);
-  const [ database, setDatabase ] = useState(null);
+  const [dashboardShowing, setDashboardShowing] = useState(false)
+  const [freeplayShowing, setFreeplayShowing] = useState(false)
+  const [createQuizShowing, setCreateQuizShowing] = useState(false)
+  const [activeQuiz, setActiveQuiz] = useState(null)
+  const [database, setDatabase] = useState(null)
 
   useEffect(() => {
-    (async () => {
-      setDatabase(await loadDatabase());
+    ;(async () => {
+      setDatabase(await loadDatabase())
     })()
-  }, [createQuizShowing]);
+  }, [createQuizShowing])
 
   useEffect(() => {
-    if (window.location.pathname[1] === "S") {
-      setActiveQuiz(JSON.parse(decodeURIComponent(window.location.pathname.slice(2))));
+    if (window.location.pathname[1] === 'S') {
+      setActiveQuiz(
+        JSON.parse(decodeURIComponent(window.location.pathname.slice(2)))
+      )
     }
-  }, []);
+  }, [])
 
   const startQuiz = (quiz) => {
-    setDashboardShowing(false);
-    setActiveQuiz(quiz);
+    setDashboardShowing(false)
+    setActiveQuiz(quiz)
   }
 
   const handleQuizSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault()
   }
 
   const switchTab = () => {
-    setCreateQuizShowing(!createQuizShowing);
+    setCreateQuizShowing(!createQuizShowing)
   }
 
   const showDashboard = () => {
@@ -67,59 +69,32 @@ const App = () => {
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar
-      toggleDashboard={showDashboard}
-      toggleFreeplay={showFreeplay}
-      logoClick={showLandingPage}
+        toggleDashboard={showDashboard}
+        toggleFreeplay={showFreeplay}
+        logoClick={showLandingPage}
       />
 
-      {
-        (!freeplayShowing) && (!dashboardShowing) && (!activeQuiz) ?
-        <LandingPage click={showDashboard} /> : null
-      }
+      {!freeplayShowing && !dashboardShowing && !activeQuiz ? (
+        <LandingPage click={showDashboard} />
+      ) : null}
 
-      {
-        freeplayShowing ? <Freeplay /> : null
-      }
+      {freeplayShowing ? <Freeplay /> : null}
 
-      {
-        dashboardShowing ?
+      {dashboardShowing ? (
         <Dashboard
           quizData={database}
           showCreateQuiz={createQuizShowing}
           switchTab={switchTab}
-          selectQuiz={quiz => startQuiz(quiz)}
-          submitQuiz={(event) => handleQuizSubmit(event)} /> : null
-      }
+          selectQuiz={(quiz) => startQuiz(quiz)}
+          submitQuiz={(event) => handleQuizSubmit(event)}
+        />
+      ) : null}
 
-      {
-        activeQuiz ?
-        <Game quiz={activeQuiz} /> : null
-      }
-
+      {activeQuiz ? <Game quiz={activeQuiz} /> : null}
     </div>
-  );
+  )
 }
 
-export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default App
--- src/components/Piano/Piano.js
+++ src/components/Piano/Piano.js
@@ -1,71 +1,98 @@
 // Libraries
-import React, { useState, useEffect } from 'react';
-import { Howler } from 'howler';
+import React, { useState, useEffect } from 'react'
+import { Howler } from 'howler'
 
 // Utility functions
-import soundPlay from '../../util/soundPlay';
+import soundPlay from '../../util/soundPlay'
 
 // Components
-import PianoNote from './PianoNote/PianoNote';
+import PianoNote from './PianoNote/PianoNote'
 
 // Data
-import audioClips from '../../data/audioClips';
-import classes from './Piano.module.css';
-
+import audioClips from '../../data/audioClips'
+import classes from './Piano.module.css'
 
 const Piano = (props) => {
-  const [controlsShowing, setControlsShowing] = useState(true);
-  const [volume, setVolume] = useState(0.9);
-  const [labelShowing, setLabelShowing] = useState(false);
+  const [controlsShowing, setControlsShowing] = useState(true)
+  const [volume, setVolume] = useState(0.9)
+  const [labelShowing, setLabelShowing] = useState(false)
   const pressNote = (note) => {
-    soundPlay(audioClips[note]);
+    soundPlay(audioClips[note])
     if (props.quiz) {
-      props.sendNoteToGame(note);
+      props.sendNoteToGame(note)
     } else if (props.form) {
-      props.sendNoteToForm(note);
+      props.sendNoteToForm(note)
     }
   }
-  let controlGear = null;
+
+  useEffect(() => {
+    Object.keys(audioClips).forEach((key) => {
+      soundPlay(audioClips[key])
+    })
+    console.log('played!')
+  }, [])
+
+  let controlGear = null
   if (!props.quiz) {
     controlGear = (
       <span
         className={classes.SettingsBtn}
-        style={{marginTop: 24, display: 'inline-block'}}
-        onClick={() => setControlsShowing(!controlsShowing)}>
-        <i class="fas fa-cog"></i>
-        {
-          (controlsShowing) ?
+        style={{ marginTop: 24, display: 'inline-block' }}
+        onClick={() => setControlsShowing(!controlsShowing)}
+      >
+        <i class='fas fa-cog'></i>
+        {controlsShowing ? (
           <div className={classes.Controls}>
             <h3>Controls</h3>
             <div>
               <p>Volume: {Math.round(volume * 100)}</p>
-              <input type="range" min="0" max="100" value={volume * 100} className={classes.Slider} onChange={e => adjustVolume(e)} />
+              <input
+                type='range'
+                min='0'
+                max='100'
+                value={volume * 100}
+                className={classes.Slider}
+                onChange={(e) => adjustVolume(e)}
+              />
             </div>
-            <button onClick={() => {setLabelShowing(!labelShowing)}}>Toggle note labels</button>
+            <button
+              onClick={() => {
+                setLabelShowing(!labelShowing)
+              }}
+            >
+              Toggle note labels
+            </button>
           </div>
-          :
-          null
-        }
+        ) : null}
       </span>
     )
   }
-  let quizControls = null;
+  let quizControls = null
   if (props.quiz) {
-    quizControls = (
-      (controlsShowing) ?
+    quizControls = controlsShowing ? (
       <div className={classes.QuizControls}>
         <h3>Controls</h3>
         <div>
           <p>Volume: {Math.round(volume * 100)}</p>
-          <input type="range" min="0" max="100" value={volume * 100} className={classes.Slider} onChange={e => adjustVolume(e)} />
+          <input
+            type='range'
+            min='0'
+            max='100'
+            value={volume * 100}
+            className={classes.Slider}
+            onChange={(e) => adjustVolume(e)}
+          />
         </div>
-        <button onClick={() => {setLabelShowing(!labelShowing)}}>Toggle note labels</button>
+        <button
+          onClick={() => {
+            setLabelShowing(!labelShowing)
+          }}
+        >
+          Toggle note labels
+        </button>
       </div>
-      :
-      null
-    );
+    ) : null
   }
-
 
   useEffect(() => {
     setControlsShowing(!controlsShowing)
@@ -78,27 +105,39 @@ const Piano = (props) => {
   Howler.volume(volume) // can make component to adjust volume
 
   return (
-    <div style={{position: 'relative'}}>
-      <div className={[classes.Piano, props.quiz ? '' : classes.FreePlayPiano].join(' ')}>
+    <div style={{ position: 'relative' }}>
+      <div
+        className={[
+          classes.Piano,
+          props.quiz ? '' : classes.FreePlayPiano,
+        ].join(' ')}
+      >
         <div className={classes.GrandPiano}>
-          {
-            Object.keys(audioClips).map((note, index) =>
-              <PianoNote
-                key={index}
-                click={() => pressNote(note)}
-                labelShow={labelShowing}
-                label={note}
-                lastAttemptData={props.attemptData ? props.attemptData[props.attemptData.length - 1] : null}
-              />
-            )
-          }
+          {Object.keys(audioClips).map((note, index) => (
+            <PianoNote
+              key={index}
+              click={() => pressNote(note)}
+              labelShow={labelShowing}
+              label={note}
+              lastAttemptData={
+                props.attemptData
+                  ? props.attemptData[props.attemptData.length - 1]
+                  : null
+              }
+            />
+          ))}
         </div>
       </div>
-    <p style={{fontSize: '14', color: '#999'}} className={classes.MobileOnly}>Swipe the keyboard to view more notes</p>
-      { controlGear }
-      { quizControls }
+      <p
+        style={{ fontSize: '14', color: '#999' }}
+        className={classes.MobileOnly}
+      >
+        Swipe the keyboard to view more notes
+      </p>
+      {controlGear}
+      {quizControls}
     </div>
-  );
+  )
 }
 
-export default Piano;
+export default Piano
