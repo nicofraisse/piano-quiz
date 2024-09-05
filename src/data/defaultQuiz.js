6ca807c487f891e65d4194b103530a5db0550cf7
Nicolas Fraisse <ni.fraisse@gmail.com>
2024-09-04 22:06:23 -0400

    Fix css issues and rephrase some captions

--- src/data/defaultQuiz.js
+++ src/data/defaultQuiz.js
@@ -1,35 +1,35 @@
 const defaultQuiz = [
   {
-    name: 'Warmup quiz with just 1 note per round',
+    name: "Warmup with 1 note per round",
     difficulty: 1,
-    quizzes: [['C2'], ['A1'], ['E2']],
+    quizzes: [["C2"], ["A1"], ["E2"]],
   },
   {
-    name: 'Beginner quiz, white keys only',
+    name: "Beginner - white keys only",
     difficulty: 1,
     quizzes: [
-      ['C1', 'D1', 'E1'],
-      ['C2', 'D2', 'E2'],
+      ["C1", "D1", "E1"],
+      ["C2", "D2", "E2"],
     ],
   },
   {
-    name: 'Quite bizarre melodies',
+    name: "Dissonant melodies",
     difficulty: 2,
     quizzes: [
-      ['F#2', 'F#1'],
-      ['D1', 'D#1', 'E1'],
-      ['G2', 'E2', 'C#2'],
+      ["F#2", "F#1"],
+      ["D1", "D#1", "E1"],
+      ["G2", "E2", "C#2"],
     ],
   },
   {
-    name: 'Your ears will likely suffer',
+    name: "Your ears might suffer",
     difficulty: 4,
     quizzes: [
-      ['F#2', 'A1', 'A#2'],
-      ['D1', 'C2', 'E1', 'F#1'],
-      ['C#1', 'E2', 'F#2', 'G#1'],
+      ["F#2", "A1", "A#2"],
+      ["D1", "C2", "E1", "F#1"],
+      ["C#1", "E2", "F#2", "G#1"],
     ],
   },
-]
+];
 
-export default defaultQuiz
+export default defaultQuiz;
--- src/layouts/LandingPage/LandingPage.js
+++ src/layouts/LandingPage/LandingPage.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import classes from './LandingPage.module.css';
+import React from "react";
+import classes from "./LandingPage.module.css";
 
 const LandingPage = (props) => {
   return (
     <div className={classes.LandingPage}>
       <div>
-        <h1>How good is your capacity to recognize notes? </h1>
-        <h2>Do you have perfect pitch, or relative pitch?</h2>
+        <h1>How good is your ability to recognize notes?</h1>
+        <h2>Do you have perfect pitch or relative pitch?</h2>
         <div onClick={props.click}>Let's find out!</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default LandingPage;
